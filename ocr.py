4c686811c352657cc450f20dbacfd34d22928690
Mubashir <116059615+forbiddenvelocity@users.noreply.github.com>
2024-06-02 19:35:19 +0530

    Update ocr.py

--- ocr.py
+++ ocr.py
@@ -4,23 +4,19 @@ from transformers import TrOCRProcessor, VisionEncoderDecoderModel
 import numpy as np
 import torch
 
-# Load the processor and model
+
 processor = TrOCRProcessor.from_pretrained('microsoft/trocr-base-printed')
 model = VisionEncoderDecoderModel.from_pretrained('microsoft/trocr-base-printed')
 
-# Function to preprocess the image
 def preprocess_image(image):
     try:
-        # Convert to RGB to ensure the image has 3 dimensions
         rgb_image = image.convert("RGB")
-        # Convert to numpy array
         np_image = np.array(rgb_image)
         return np_image
     except Exception as e:
         st.error(f"Error in preprocessing image: {e}")
         return None
-
-# Function to extract text from image using the OCR model
+        
 def extract_text_from_image(image):
     try:
         pixel_values = processor(images=image, return_tensors="pt").pixel_values
@@ -31,7 +27,6 @@ def extract_text_from_image(image):
         st.error(f"Error in extracting text from image: {e}")
         return None
 
-# Main function for the Streamlit app
 def main():
     st.title("CAPTCHA Text Extractor")
     uploaded_file = st.file_uploader("Choose a CAPTCHA image...", type=["jpg", "jpeg", "png"])
